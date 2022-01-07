0d8da65a977a50a1566ebfed208cd2442602e627
ponomvv <ponomvv@gmail.com>
2022-01-07 19:03:22 +0300

    Использование порядковых номеров.

--- 07/js/index-numbers.js
+++ 07/js/index-numbers.js
@@ -1,5 +1,5 @@
-$(function() {
-  $('li:lt(2)').removeClass('hot');
-  $('li').eq(0).addClass('complete');
-  $('li:gt(2)').addClass('cool');
+$ (function () {
+    $ ('li:lt(2)').removeClass ('hot');
+    $ ('li').eq (0).addClass ('complete');
+    $ ('li:gt(2)').addClass ('cool');
 });
