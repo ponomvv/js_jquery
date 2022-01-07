c335575a7c452b734aed5dfb290e2e167ffb2c35
ponomvv <ponomvv@gmail.com>
2022-01-07 14:54:42 +0300

    Обход дерева DOM.

--- 07/js/animate.js
+++ 07/js/animate.js
@@ -1,1 +1,1 @@
-$(function() {  $('li').on('click', function() {    $(this).animate({      opacity: 0.0,      paddingLeft: '+=80'    }, 500, function() {      $(this).remove();    });  });});
+$ (function () {    $ ('li').on ('click', function () {        $ (this).animate ({                opacity: 0.0,                paddingLeft: '+=80'            },            900, function () {                $ (this).remove ();            });    });});
--- 07/js/event-delegation.js
+++ 07/js/event-delegation.js
@@ -1,16 +1,14 @@
-﻿$(function() {
-  var listItem, itemStatus, eventType;
-
-  $('ul').on(
-    'click mouseover',
-    ':not(#four)',
-    {status: 'important'},
-    function(e) {
-      listItem = 'Элемент: ' + e.target.textContent + '<br />';
-      itemStatus = 'Состояние: ' + e.data.status + '<br />';
-      eventType = 'Событие: ' + e.type;
-      $('#notes').html(listItem + itemStatus + eventType);
-    }
-  );
-
+﻿$ (function () {
+    var listItem, itemStatus, eventType;
+    $ ('ul').on (
+        'click mouseover',
+        ':not(#four)',
+        {status: 'important'},
+        function (e) {
+            listItem = 'Элемент: ' + e.target.textContent + '<br/>';
+            itemStatus = 'Состояние: ' + e.data.status + '<br/>';
+            eventType = 'Событие: ' + e.type;
+            $ ('#notes').html (listItem + itemStatus + eventType);
+        }
+    );
 });
